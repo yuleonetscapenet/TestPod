88e33668d3ec54cb1be3d2644c9ebe73820f4671
Yu leo <yuleo@netscape.net>
2020-08-23 14:19:00 -0700

    Initial commit

---
+++ KnobControl/KnobControl.h
@@ -0,0 +1,19 @@
+//
+//  KnobControl.h
+//  KnobControl
+//
+//  Created by Leo Yu on 8/22/20.
+//  Copyright © 2020 self. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KnobControl.
+FOUNDATION_EXPORT double KnobControlVersionNumber;
+
+//! Project version string for KnobControl.
+FOUNDATION_EXPORT const unsigned char KnobControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KnobControl/PublicHeader.h>
+
+
---
+++ Pods/Target Support Files/Pods-KnobControl/Pods-KnobControl-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KnobControl : NSObject
+@end
+@implementation PodsDummy_Pods_KnobControl
+@end
